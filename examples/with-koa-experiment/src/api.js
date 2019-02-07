92e2f4bddc736084291409c0af1af16fccd10e86
giiorg <giorgi.bichinashvili@gmail.com>
2019-02-07 19:22:57 +0400

    feat: graphql engine as a foundation for routes

--- examples/with-koa-experiment/src/api.js
+++ examples/with-koa-experiment/src/api.js
@@ -5,8 +5,22 @@ const api = new ApiComposer()
 
 api.setResolversPath(path.resolve(__dirname, 'resolvers'))
 
-api.query('me', 'me')
+api.createType('mutationType', {
+  a: 'String',
+  b: 'String',
+})
+
+api.query('me', async () => 'ME!')
+
+api
+  .mutation('simpleMutation')
+  .resolve('simpleMutation')
+  .args({
+    arg1: 'String!'
+  })
+  .type('mutationType')
 
 module.exports = {
-  koaRouter: api.getKoaRouter()
+  koaRouter: api.getKoaRouter(),
+  graphqlSchema: api.getGraphqlSchema()
 }
--- examples/with-koa-experiment/src/app.js
+++ examples/with-koa-experiment/src/app.js
@@ -1,14 +1,25 @@
 const Koa = require('koa')
 const koaBody = require('koa-body')
-const { koaRouter } = require('./api')
+const Router = require('koa-router')
+const graphqlHTTP = require('koa-graphql')
+const { koaRouter, graphqlSchema } = require('./api')
 
 const app = new Koa()
+app.use(koaBody())
+
+const router = new Router()
+router.all('/graphql', graphqlHTTP({
+  schema: graphqlSchema,
+  graphiql: true
+}))
 
 app
-  .use(koaBody())
-
+  .use(router.routes())
+  .use(router.allowedMethods())
+  
 app
   .use(koaRouter.routes())
+  .use(koaRouter.allowedMethods())
 
 app.listen('8000', () => {
   console.log('app running on 8000')
